fa0d5b495958fa8f47c537d8bf2763f484dce243
RalenskiDoucet <Ralenskid@gmail.com>
2017-12-11 12:57:52 -0600

    Start Algorithms Course

---
+++ Algrorthims example/Examples.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<assert.h>
+
+////Longest Value in the collection of.
+//
+//int SearchLargest(int*Collection, int numberofItems)
+//{
+//	//check if collection is empty.
+//	if(numberofItems, numberofItems <= 0,numberofItems++);
+//	// Assume 1st number in  the set is the largest.
+//	//Create new Variable of type int.
+//	int LargestNum;
+//	//Create a loop that iterates over the collection.
+//	//if current value is greater than current largest.
+//	if (LargestNum, LargestNum >= 0,LargestNum++);
+//	//int LargestNum is assigned to current value.
+//	for (LargestNum; LargestNum = 1; LargestNum++)
+//	{
+//
+//	}
+//
+//	//return Largest value.
+//}
+class Cell
+{
+public:
+	char *m_Token;
+	Cell()
+	{
+		m_Token[0];
+	};
+	Cell(char *Token)
+	{
+		m_Token = Token;
+	};
+	bool operator==(Cell&other)
+	{
+		if (m_Token == other.m_Token)
+		{
+			return true;
+		}
+		if (m_Token != other.m_Token)
+		{
+			return false;
+		}
+
+	};
+};
+class Column
+{
+public:
+	int m_Height;
+	Cell *m_Cells;
+	Column()
+	{
+		m_Cells[7];
+	}
+	Column(int Height)
+	{
+		m_Height=Height;
+
+	}
+	int mLastOccupied(int)
+	{
+		int i;
+		for (m_Cells[i]; i < 0; i++)
+		{
+			return i;
+		}
+		//
+		//	}
+		//
+		//	bool isFull()
+		//	{
+		//		int i;
+		//		if (m_Cells[15])
+		//	}
+		//	//bool PlaceToken(char Token)
+		//	//{
+		//	//	Cell m_Cells;
+		//	//	for (m_Cells;);
+		//
+		//	//}
+		//};
+		//int main()
+		//{
+		//
+		//	Cell testNum1;	Cell testNum2;
+		//	bool test1 =  testNum1 == testNum2;
+		//	assert((testNum1==);
+	}
+};
+		class ConnectFour
+		{
+		public:
+			Column *m_Column()
+			{
+				Cell m_Cells[6];
+				m_Cells[6];
+			}
+			char ActiveToken;
+			int m_NumofCols;
+			int *m_ColHeigth;
+			ConnectFour(int *numOfCols, int Height)
+			{
+				m_NumofCols = numOfCols[7];
+				m_ColHeigth[6] = Height;
+				for (ActiveToken; PlacePiece;)
+			{
+
+			}
+
+			}
+			bool PlacePiece(int col)
+			{
+
+			}
+			bool Update()
+			{
+
+			}
+			bool CheckVictory()
+			{
+
+			}
+		};
